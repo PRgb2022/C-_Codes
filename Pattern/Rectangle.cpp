d15de34d6bcc0187ff71929bddcb43fc6a300d53
Prajwal Kumar Sahu <prajwalkumarsahu@Prajwals-MacBook-Air.local>
2023-02-22 15:55:19 +0530

    C++: Patters

---
+++ Pattern/Rectangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b;
+    cout<<"Enter the length of the rectangle: ";
+    cin>>l;
+    cout<<"Enter the breadth of the rectangle: ";
+    cin>>b;
+    for(int i=0;i<l;i++)
+    {
+        for(int j=0;j<b;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
